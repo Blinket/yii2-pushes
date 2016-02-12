4a424d3ce81bad371710a1cd971afefe0bc39155
Alexander Kryshtalev <alex@voodoo-mobile.com>
2016-02-12 12:14:06 +0600

    [+] Migrated code from yii2vm

---
+++ src/PushProvider.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alex
+ * Date: 07/05/15
+ * Time: 22:35
+ */
+
+namespace vm\pushes;
+
+use yii\base\Component;
+
+/**
+ * Class PushProvider
+ * @package vm\pushes
+ */
+abstract class PushProvider extends Component
+{
+    /**
+     * @return PushMessage
+     */
+    public function compose()
+    {
+        return new PushMessage([
+            'provider' => $this
+        ]);
+    }
+
+    /**
+     * @param PushMessage $message
+     *
+     * @return mixed
+     */
+    public abstract function deliver(PushMessage $message);
+}
